fc5317b678992f9f5af5172dd1fe8964754fbffa
Jerry Chen <caffreygo@163.com>
2022-09-20 23:37:01 +0800

    user register to database

--- nest/src/auth/auth.service.ts
+++ nest/src/auth/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@nestjs/common'
+import { hash } from 'argon2'
+import { PrismaService } from 'src/prisma/prisma.service'
+import RegisterDto from './dto/register.dto'
 
 @Injectable()
 export class AuthService {
-  register(dto) {
-    console.log(dto)
-    return 123
+  constructor(private prisma: PrismaService) {}
+
+  async register(dto: RegisterDto) {
+    const user = await this.prisma.user.create({
+      data: {
+        name: dto.name,
+        password: await hash(dto.password),
+      },
+    })
+    return user
+  }
+
+  private token() {
+    return {
+      token: '',
+    }
   }
 }
