bcf0f779da74edbe141276fb57c62747c731adec
Jerry Chen <caffreygo@163.com>
2022-10-02 15:19:38 +0800

    global api prefix

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
 import Validate from './common/validate'
 import { TransformInterceptor } from './transform.inteceptor'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.useGlobalPipes(new Validate())
   app.useGlobalInterceptors(new TransformInterceptor())
+  app.setGlobalPrefix('api')
   await app.listen(3000)
 }
 bootstrap()
