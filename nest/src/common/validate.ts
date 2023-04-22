d2edfc7f4bd8dddf2b90ee9d2c30ca868ddfcec8
Jinrui <jerry.chenj@outlook.com>
2023-04-22 23:12:56 +0800

    Update validate.ts

--- nest/src/common/validate.ts
+++ nest/src/common/validate.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, ValidationPipe } from '@nestjs/common'
 import { ValidationError } from 'class-validator'
 
+// 对因为数据类型校验而返回的错误进行统一
 export default class Validate extends ValidationPipe {
   protected flattenValidationErrors(validationErrors: ValidationError[]): string[] {
     const messages = {}
